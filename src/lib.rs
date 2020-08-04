e8b42a6ad4c1015c963b14252ba86ac4f7157038
quant-shiva <shivamtind@gmail.com>
2020-08-04 20:46:27 +0530

    text searching complete

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+use std::fs;
+use std::error::Error;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String    
+}
+
+impl Config {
+pub fn new(args: &Vec<String>) -> Result<Config, &'static str> {
+    if args.len() < 3 {
+        return Err("Not enough arguments.");
+    }
+    let query: String = args[1].clone();
+    let filename: String = args[2].clone();
+
+    Ok(Config {query,filename} )
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content: String = fs::read_to_string(config.filename)?;
+
+    for line in search(&config.query, &content) {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results  = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process;
+
+use mini_grep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem in parsing aurgument: {}", err);
+        process::exit(1);
+    });
+
+    println!("{}",config.query);
+    println!("{}",config.filename);
+
+    if let Err(e) = mini_grep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
